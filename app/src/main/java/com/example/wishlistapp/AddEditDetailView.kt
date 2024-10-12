5fceee2d990e011cd5f87ae6f56e823ae965e03d
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-10-12 23:37:07 +0700

    added WishTextField to the Scaffold Column

--- app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
+++ app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material3.Button
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,9 +22,11 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import com.example.wishlistapp.data.Wish
 
 @Composable
 fun AddEditDetailView(
@@ -46,6 +49,44 @@ fun AddEditDetailView(
             verticalArrangement = Arrangement.Center,
         ) {
             Spacer(modifier = Modifier.height(10.dp))
+
+            // Title
+            WishTextField(
+                label = "Title",
+                value = viewModel.wishTitleState,
+                onValueChange = { it ->
+                    viewModel.onWishTitleChange(it)
+                }
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // Description
+            WishTextField(
+                label = "Description",
+                value = viewModel.wishDescriptionState,
+                onValueChange = { it ->
+                    viewModel.onWishDescriptionChange(it)
+                }
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Button(onClick = {
+                if(viewModel.wishTitleState.isNotEmpty() &&
+                    viewModel.wishDescriptionState.isNotEmpty()
+                    ) {
+                    // TODO UpdateWish
+                }
+                else {
+                    // TODO AddWish
+                }
+            }) {
+                Text(
+                    title,
+                    fontSize = 18.sp,
+                )
+            }
         }
     }
 }
