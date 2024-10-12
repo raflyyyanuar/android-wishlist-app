1cad6a7adedac7e1d2a989ef07d54ac011bd2174
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-10-12 21:59:07 +0700

    added add button

--- app/src/main/java/com/example/wishlistapp/HomeView.kt
+++ app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -4,10 +4,17 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView() {
@@ -23,7 +30,27 @@ fun HomeView() {
                         Toast.LENGTH_SHORT
                     ).show()
                 }
-            )}
+            )},
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    Toast.makeText(
+                        context,
+                        "Add Button Clicked!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                },
+                modifier = Modifier
+                    .padding(20.dp),
+                contentColor = colorResource(id = R.color.dark_black),
+                backgroundColor = colorResource(id = R.color.orange)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null
+                )
+            }
+        }
     ) {
         LazyColumn(
             modifier = Modifier
