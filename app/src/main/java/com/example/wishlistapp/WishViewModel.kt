b952a33753020bd7628130cd514eda99efabe687
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-10-12 23:36:43 +0700

    added basic title and description change

--- app/src/main/java/com/example/wishlistapp/WishViewModel.kt
+++ app/src/main/java/com/example/wishlistapp/WishViewModel.kt
@@ -1,9 +1,20 @@
 package com.example.wishlistapp
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.wishlistapp.data.Wish
 
 class WishViewModel : ViewModel() {
-//    private val _wish = mutableStateOf(Wish)
+    var wishTitleState by mutableStateOf("")
+    var wishDescriptionState by mutableStateOf("")
+
+    fun onWishTitleChange(newTitle: String) {
+        wishTitleState = newTitle
+    }
+
+    fun onWishDescriptionChange(newDescription: String) {
+        wishDescriptionState = newDescription
+    }
 }
