eea1e46aa16b7d01b71c1474246896a29d0b94a0
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-10-12 23:26:39 +0700

    added WishTextField

--- app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
+++ app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
@@ -3,14 +3,23 @@ package com.example.wishlistapp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
@@ -38,5 +47,49 @@ fun AddEditDetailView(
         ) {
             Spacer(modifier = Modifier.height(10.dp))
         }
+    }
+}
+
+@Composable
+fun WishTextField(
+    label : String,
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = {
+            Text(
+                label,
+                color = colorResource(id = R.color.white)
+            )
+        },
+        modifier = Modifier
+            .fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            // text
+            textColor = colorResource(id = R.color.white),
+
+            // border
+            focusedBorderColor = colorResource(id = R.color.white),
+            unfocusedBorderColor = colorResource(id = R.color.white),
+
+            // label
+            focusedLabelColor = colorResource(id = R.color.white),
+            unfocusedLabelColor = colorResource(id = R.color.white),
+
+            // cursor
+            cursorColor = colorResource(id = R.color.white),
+        )
+    )
+}
+
+@Preview(showBackground = true, backgroundColor = 0x000000)
+@Composable
+fun WishTextFieldPreview() {
+    WishTextField(label = "This is a label", value = "This is a value") {
+        
     }
 }
